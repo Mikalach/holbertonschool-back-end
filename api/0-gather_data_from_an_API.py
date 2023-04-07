ce39d1ea2d1b45f6f05dec8686f4e899c416da62
Mikalach <mika.djetic@gmaiul.com>
2023-04-07 02:12:59 -0700

    did not understand how the checker worked

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
 """
-Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+Fetches todo list progress for a given employee ID from a REST API
 """
 
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    # Check for correct number of arguments
     if len(sys.argv) != 2:
-        print("Usage: {} employee_id".format(sys.argv[0]))
+        print("Usage: ./todo.py <employee_id>")
         sys.exit(1)
 
-    # Get employee ID from command line argument
-    employee_id = sys.argv[1]
+    employee_id = int(sys.argv[1])
 
-    # Get employee name from API
-    url_user = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    response_user = requests.get(url_user)
-    if response_user.status_code != 200:
-        print("Error: Employee ID not found")
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+    # Fetch user information
+    user = None
+    resp = requests.get(users_url, params={"id": employee_id})
+    if resp.ok:
+        user = resp.json()[0]
+
+    if not user:
+        print("Employee not found")
         sys.exit(1)
-    employee_name = response_user.json().get("name")
 
-    # Get employee's TODO list from API
-    url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
-    response_todos = requests.get(url_todos)
-    if response_todos.status_code != 200:
-        print("Error: Something went wrong")
-        sys.exit(1)
-    todos = response_todos.json()
+    # Fetch user's TODO list
+    todos = []
+    resp = requests.get(todos_url, params={"userId": employee_id})
+    if resp.ok:
+        todos = resp.json()
 
-    # Process TODO list and display progress
     total_tasks = len(todos)
-    completed_tasks = [t for t in todos if t.get("completed")]
-    num_completed_tasks = len(completed_tasks)
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed_tasks, total_tasks))
-    for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
+    done_tasks = sum(1 for todo in todos if todo["completed"])
+    task_titles = [todo["title"] for todo in todos if todo["completed"]]
+
+    # Print output
+    print("Employee {} is done with tasks({}/{}):".format(
+        user["name"], done_tasks, total_tasks))
+    for title in task_titles:
+        print("\t {}".format(title))
