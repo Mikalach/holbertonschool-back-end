cad8c19ba7ce167d025caf3fdb35a9829c5a2f27
Mikalach <mika.djetic@gmaiul.com>
2023-04-07 06:11:08 -0700

    insane

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""documented"""
+import csv
+import json
+import requests
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+    # Fetch employee name
+    users_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(users_url)
+    employee_name = response.json()["username"]
+
+    # Fetch todo list for the employee
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    query = {"userId": employee_id}
+    response = requests.get(todos_url, params=query)
+
+    todo_list = response.json()
+
+    # Export data to CSV file
+    with open(f"{employee_id}.csv", "w") as f:
+        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todo_list:
+            csv_writer.writerow([employee_id, employee_name,
+                                 task["completed"], task["title"]])
+
+    return todo_list
+
+
+if __name__ == "__main__":
+    all_todo_data = {}
+
+    for employee_id in range(1, 11):
+        todo_list = get_employee_todo_progress(employee_id)
+        all_todo_data[employee_id] = []
+        for task in todo_list:
+            all_todo_data[employee_id].append(
+                {"username": task["userId"],
+                 "task": task["title"],
+                 "completed": task["completed"]}
+            )
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(all_todo_data, f)
+
+    print("Todo list exported to todo_all_employees.json")
